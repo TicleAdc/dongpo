f2f7b52b9750e97e1f85ebd9e86f73e0c37456e2
ztzh_ranzhic <ztzh_ranzhic@ztzh.com>
2020-09-22 09:51:44 +0800

    样式修改

--- src/views/SpecialActivity.vue
+++ src/views/SpecialActivity.vue
@@ -2,7 +2,7 @@
  * @Author: Spring Breeze
  * @Date: 2020-09-17 11:13:06
  * @FilePath: /dongpo/src/views/SpecialActivity.vue
- * @LastEditTime: 2020-09-17 11:14:57
+ * @LastEditTime: 2020-09-22 09:49:38
 -->
 <template>
   <div class="body">
