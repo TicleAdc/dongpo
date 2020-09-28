7a99d863b0350158d79c80fe4c55d3fbcac7698c
ztzh_ranzhic <ztzh_ranzhic@ztzh.com>
2020-09-28 09:55:40 +0800

    公共组件

---
+++ src/components/common/msgList.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Author: Spring Breeze
+ * @Date: 2020-09-28 09:54:49
+ * @FilePath: /dongpo/src/components/common/msgList.vue
+ * @LastEditTime: 2020-09-28 09:54:52
+-->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped></style>
--- src/components/hospitalannounc.vue
+++ src/components/hospitalannounc.vue
@@ -2,14 +2,22 @@
  * @Author: Spring Breeze
  * @Date: 2020-09-28 09:13:30
  * @FilePath: /dongpo/src/components/hospitalannounc.vue
- * @LastEditTime: 2020-09-28 09:38:58
+ * @LastEditTime: 2020-09-28 09:55:33
+ * @description 医院公告组件
 -->
 <template>
-  <div>12312</div>
+  <div>
+    <msg-list></msg-list>
+  </div>
 </template>
 
 <script>
-export default {};
+import msgList from './common/msgList';
+export default {
+  components: {
+    msgList,
+  },
+};
 </script>
 
 <style lang="less" scoped></style>
