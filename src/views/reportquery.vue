c07100b178d18278375f7f87bf6e415bb092ce81
ztzh_gancy <gancy0715@zsmarter.com>
2020-09-28 10:51:17 +0800

    新加页面

---
+++ src/views/reportquery.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>报告查询</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    this.getInfo()
+  },
+  methods: {
+    getData() {
+
+    },
+
+    async getInfo() {
+      await this.http.post().then({
+        
+      })
+    }
+  },
+};
+</script>
