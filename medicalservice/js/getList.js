26a37edd609dba0ab0443d2ff8bee4878f3433dc
ztzh_ranzhic <ztzh_ranzhic@ztzh.com>
2020-09-30 15:45:45 +0800

    医疗服务

--- medicalservice/js/getList.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: Spring Breeze
- * @Date: 2020-09-30 15:08:43
- * @FilePath: /dongpo/medicalservice/js/getList.js
- * @LastEditTime: 2020-09-30 15:28:46
- */
-// 从服务器请求list
-
-$('msg-list');
