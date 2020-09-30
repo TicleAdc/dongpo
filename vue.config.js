d2d0fb78c6925e91d516b51b1af39c14efcc1931
ztzh_ranzhic <ztzh_ranzhic@ztzh.com>
2020-09-30 11:21:35 +0800

    1

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: Spring Breeze
+ * @Date: 2020-09-30 11:21:30
+ * @FilePath: /dongpo/vue.config.js
+ * @LastEditTime: 2020-09-30 11:21:32
+ */
+module.exports = {
+  publicPath: '',
+};
