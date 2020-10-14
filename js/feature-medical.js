bc303199f119a5524e2962565e2f9ac3faf01ea7
ztzh_ranzhic <ztzh_ranzhic@ztzh.com>
2020-10-14 21:03:08 +0800

    bug

--- js/feature-medical.js
+++ js/feature-medical.js
@@ -7,14 +7,47 @@ var feature = {
     for (var i = 0; i < li.length; i++)
       li[i].onclick = function () {
         for (var i = 0; i < li.length; i++) li[i].className = '';
-        this.className = 'active'
+        this.className = 'active';
         // document.querySelectorAll('.ks_title')[0].innerHTML = this.innerHTML
-        var name = document.querySelectorAll('.ks_name')
-        var text = this.innerHTML
+        var name = document.querySelectorAll('.ks_name');
+        var text = this.innerHTML;
         for (var h = 0; h < name.length; h++) {
-          name[h].innerHTML = text
+          name[h].innerHTML = text;
         }
-      }
-  }
+      };
+  },
 };
 feature.init();
+if (document.querySelector('#ks_title')) {
+  document.querySelector('#ks_title').onclick = function () {
+    var menu = document.querySelector('#menu');
+    if (getComputedStyle(menu).display === 'none') {
+      menu.style.display = 'block';
+    } else {
+      menu.style.display = 'none';
+    }
+  };
+  document.documentElement.onclick = function (e) {
+    var ksTitle = document.querySelector('#ks_title');
+    var menu = document.querySelector('#menu');
+    if (e.target !== ksTitle) {
+      menu.style.display = 'none';
+    }
+  };
+} else if (document.querySelector('#path-nav')) {
+  document.querySelector('#path-nav').onclick = function () {
+    var menu = document.querySelector('#menu');
+    if (getComputedStyle(menu).display === 'none') {
+      menu.style.display = 'block';
+    } else {
+      menu.style.display = 'none';
+    }
+  };
+  document.documentElement.onclick = function (e) {
+    var ksTitle = document.querySelector('#path-nav');
+    var menu = document.querySelector('#menu');
+    if (e.target !== ksTitle) {
+      menu.style.display = 'none';
+    }
+  };
+}
